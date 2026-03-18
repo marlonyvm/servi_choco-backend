7ab98d3c63fb9b7f1fdfc678dd0f6354f582a4c0
marlonyvm <yesithmontoya@gmail.com>
2026-03-17 20:05:36 -0500

    fix: puerto railway1

--- server/server.js
+++ server/server.js
@@ -128,7 +128,6 @@ app.get("/", (req, res) => {
 ================================ */
 
 
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
